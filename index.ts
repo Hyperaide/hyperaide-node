c4dcefc9ea3e7d2b8cd1f132ea6442d911936e90
John Paul <johnarpaul@gmail.com>
2023-10-24 12:43:27 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -41,28 +41,28 @@ class Vectors {
     this.hyperaide = hyperaide;
   }
 
-  async add(options: { text: string, metadata?: any }) {
+  async add(options: { vectorBoxId: string, text: string, metadata?: any }) {
     const response = await axios.post(`${this.hyperaide.baseURL}/api/v1/vector`, convertKeysToSnakeCase(options), { headers: this.hyperaide.getHeaders() });
     return response.data;
   }
 
-  async update(options: { vectorId: string, text: string, metadata?: any }) {
+  async update(options: { vectorBoxId: string, vectorId: string, text: string, metadata?: any }) {
     const { vectorId, ...rest } = options;
     const response = await axios.patch(`${this.hyperaide.baseURL}/api/v1/vector`, rest, { headers: this.hyperaide.getHeaders() });
     return response.data;
   }
 
-  async delete(options: { vectorId: string }) {
+  async delete(options: { vectorBoxId: string, vectorId: string }) {
     const response = await axios.delete(`${this.hyperaide.baseURL}/api/v1/vector`, { data: convertKeysToSnakeCase(options), headers: this.hyperaide.getHeaders() });
     return response.data;
   }
 
-  async show(options: { vectorId: string }) {
+  async retrieve(options: { vectorBoxId: string, vectorId: string }) {
     const response = await axios.get(`${this.hyperaide.baseURL}/api/v1/vector`, { params: convertKeysToSnakeCase(options), headers: this.hyperaide.getHeaders() });
     return response.data;
   }
 
-  async search(options: { query: string }) {
+  async search(options: { vectorBoxId: string, query: string }) {
     const response = await axios.post(`${this.hyperaide.baseURL}/api/v1/vector_box/search`, convertKeysToSnakeCase(options), { headers: this.hyperaide.getHeaders() });
     return response.data;
   }
